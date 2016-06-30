29841a0608e7d694f9126c77b1770091928277db
Nicolas Poirier-Barolet <picolo@la-grange.ca>
2016-06-30 16:54:44 -0400

    Working

--- src/main.js
+++ src/main.js
@@ -1,26 +1,15 @@
-
-
+import React from 'react';
+import { render } from 'react-dom';
+import { Provider } from 'react-redux';
 import { createStore } from 'redux';
 import todoApp from './reducers/todos';
-
-import { addTodo, toggleTodo, setVisibilityFilter, VisibilityFilters } from './actions/todos';
-
+import App from './components/App';
 
 let store = createStore(todoApp);
 
-/*
-// TEST APP 
-let unsubscribe = store.subscribe(() => {
-    console.log(store.getState());
-});
-
-// Dispatch some actions
-store.dispatch(addTodo('Learn about actions'));
-store.dispatch(addTodo('Learn about reducers'));
-store.dispatch(addTodo('Learn about store'));
-store.dispatch(toggleTodo(1));
-store.dispatch(toggleTodo(2));
-store.dispatch(setVisibilityFilter(VisibilityFilters.SHOW_COMPLETED));
-
-unsubscribe();
-/**/
+render(
+    <Provider store={store}>
+        <App />
+    </Provider>,
+    document.getElementById('root')
+);
